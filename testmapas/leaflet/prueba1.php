8d26748da8a360ff3cd898643da1220f53491f7a
LeaxGuidi <lg.leax@gmail.com>
2014-10-04 15:52:14 -0300

    archivo prueba vista de mapas

---
+++ testmapas/leaflet/prueba1.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+	<title>Test Mapas</title>
+	<link rel="stylesheet" type="text/css" href="leaflet.css">
+	<script type="text/javascript" href="leaflet.js"></script>
+</head>
+<body>
+	<div id="map"></div>
+</body>
+</html>
