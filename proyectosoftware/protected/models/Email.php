fa45f3504cfff2fa4f9c4960182172b676c02e0f
LeaxGuidi <lg.leax@gmail.com>
2014-10-11 16:27:36 -0300

    Luciano implemento el patron adapter

--- proyectosoftware/protected/models/Email.php
+++ proyectosoftware/protected/models/Email.php
@@ -2,124 +2,62 @@
 
 Yii::import('application.extensions.phpmailer.JPhpMailer');
 
+/**
+ * 
+ */
+Class ssss {
+		
+		public function __construct($subject,$message){
+			$mail = new JPhpMailer;
+			//Definir que vamos a usar SMTP
+			$mail->IsSMTP();
+			//Esto es para activar el modo depuración. En entorno de pruebas lo mejor es 2, en producción siempre 0
+			// 0 = off (producción)
+			// 1 = client messages
+			// 2 = client and server messages
+			$mail->SMTPDebug = 0;
+			//Ahora definimos gmail como servidor que aloja nuestro SMTP
+			$mail->Host = 'mx1.hostinger.com.ar';
+			//El puerto
+			$mail->Port = 2525;
+			//Definmos la seguridad como TLS
+			//~ $mail->SMTPSecure = 'tls';
+			//Tenemos que usar gmail autenticados, así que esto a TRUE
+			$mail->SMTPAuth = true;
+			//Definimos la cuenta que vamos a usar. Dirección completa de la misma
+			$mail->Username = "contacto@unaj-informatica.com.ar";
+			//Introducimos nuestra contraseña
+			$mail->Password = "user--EMAIL999";
+			$mail->Subject = $subject;
+			$mail->MsgHTML($message);
+			//Y por si nos bloquean el contenido HTML (algunos correos lo hacen por seguridad) una versión alternativa en texto plano (también será válida para lectores de pantalla)
+			$mail->AltBody = $message;
+		}
 
+		public function enviar($emailRem,$nombreRem,$emailTo,$nombreTo){
+			$mail->SetFrom($emailRem, $nombreRem);
+			$mail->AddAddress($emailTo, $nombreTo);	
+			//Enviamos el correo
+			$mail->Send();
+		}
+	}
+
+#PATRON ADAPTER CHAMUYO 
 class Email {
 	
 	public static function enviar($to, $subject, $message) {
-		
-		$mail = new JPhpMailer;
-		
-		//Definir que vamos a usar SMTP
-		$mail->IsSMTP();
-		
-		//Esto es para activar el modo depuración. En entorno de pruebas lo mejor es 2, en producción siempre 0
-		// 0 = off (producción)
-		// 1 = client messages
-		// 2 = client and server messages
-		$mail->SMTPDebug = 0;
-		
-		//Ahora definimos gmail como servidor que aloja nuestro SMTP
-		$mail->Host = 'mx1.hostinger.com.ar';
-		
-		//El puerto
-		$mail->Port = 2525;
-		
-		//Definmos la seguridad como TLS
-		//~ $mail->SMTPSecure = 'tls';
-		
-		//Tenemos que usar gmail autenticados, así que esto a TRUE
-		$mail->SMTPAuth = true;
-		
-		//Definimos la cuenta que vamos a usar. Dirección completa de la misma
-		$mail->Username = "contacto@unaj-informatica.com.ar";
-		
-		//Introducimos nuestra contraseña
-		$mail->Password = "user--EMAIL999";
-		
-		//Definimos el remitente (dirección y, opcionalmente, nombre)
-		$mail->SetFrom('contacto@unaj-informatica.com.ar', 'UNAJ - Proyecto de Software');
-		
-		//Esta línea es por si queréis enviar copia a alguien (dirección y, opcionalmente, nombre)
-		//~ $mail->AddReplyTo('replyto@correoquesea.com','El de la réplica');
-		
-		//Y, ahora sí, definimos el destinatario (dirección y, opcionalmente, nombre)
-		$mail->AddAddress($to, 'usuario');
-		
-		//Definimos el tema del email
-		$mail->Subject = $subject;
-		
-		//Para enviar un correo formateado en HTML lo cargamos con la siguiente función. Si no, puedes meterle directamente una cadena de texto.
-		//~ $mail->MsgHTML(file_get_contents('correomaquetado.html'), dirname(ruta_al_archivo));
-		$mail->MsgHTML($message);
-		
-		//Y por si nos bloquean el contenido HTML (algunos correos lo hacen por seguridad) una versión alternativa en texto plano (también será válida para lectores de pantalla)
-		$mail->AltBody = $message;
-		
-		//Enviamos el correo
-		$mail->Send();
-
-				
+		$ss = new ssss($subject, $message);
+		//Definimos el remitente (dirección y, opcionalmente, nombre)		
+		$ss->enviar('contacto@unaj-informatica.com.ar', 'UNAJ - Proyecto de Software', $to, 'usuario');		
 	}
 	
 	
 	public static function recibir_contacto($to, $name, $subject, $message) {
-		
-		$mail = new JPhpMailer;
-		
-		//Definir que vamos a usar SMTP
-		$mail->IsSMTP();
-		
-		//Esto es para activar el modo depuración. En entorno de pruebas lo mejor es 2, en producción siempre 0
-		// 0 = off (producción)
-		// 1 = client messages
-		// 2 = client and server messages
-		$mail->SMTPDebug = 0;
-		
-		//Ahora definimos gmail como servidor que aloja nuestro SMTP
-		$mail->Host = 'mx1.hostinger.com.ar';
-		
-		//El puerto
-		$mail->Port = 2525;
-		
-		//Definmos la seguridad como TLS
-		//~ $mail->SMTPSecure = 'tls';
-		
-		//Tenemos que usar gmail autenticados, así que esto a TRUE
-		$mail->SMTPAuth = true;
-		
-		//Definimos la cuenta que vamos a usar. Dirección completa de la misma
-		$mail->Username = "contacto@unaj-informatica.com.ar";
-		
-		//Introducimos nuestra contraseña
-		$mail->Password = "user--EMAIL999";
-		
-		//Definimos el remitente (dirección y, opcionalmente, nombre)
-		$mail->SetFrom($to, $name.' ('.$to.')');
-		
-		//Esta línea es por si queréis enviar copia a alguien (dirección y, opcionalmente, nombre)
-		//~ $mail->AddReplyTo('replyto@correoquesea.com','El de la réplica');
-		
-		//Y, ahora sí, definimos el destinatario (dirección y, opcionalmente, nombre)
-		$mail->AddAddress('contacto@unaj-informatica.com.ar', '');
-		
-		//Definimos el tema del email
-		$mail->Subject = $subject;
-		
-		//Para enviar un correo formateado en HTML lo cargamos con la siguiente función. Si no, puedes meterle directamente una cadena de texto.
-		//~ $mail->MsgHTML(file_get_contents('correomaquetado.html'), dirname(ruta_al_archivo));
-		$mail->MsgHTML($message);
-		
-		//Y por si nos bloquean el contenido HTML (algunos correos lo hacen por seguridad) una versión alternativa en texto plano (también será válida para lectores de pantalla)
-		$mail->AltBody = $message;
-		
-		//Enviamos el correo
-		$mail->Send();
-
-				
+		$ss = new ssss($subject, $message);
+		//Definimos el remitente (dirección y, opcionalmente, nombre)		
+		$ss->enviar($to, $name.' ('.$to.')', $to, 'usuario', 'contacto@unaj-informatica.com.ar', '');
 	}
-	
-	
-	
+
 }
 
 
