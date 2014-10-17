10b50166f7a09ea6231d96d7cf3493711669aae7
Roberto Devicenzi <rdevicenzi@gmail.com>
2014-10-16 23:21:35 -0300

    se descomenta el componentes urlManager para trabajar con urls limpias

--- proyectosoftware/protected/config/main.php
+++ proyectosoftware/protected/config/main.php
@@ -48,40 +48,46 @@ return array(
 			'allowAutoLogin'=>true,
 		),		
 		// uncomment the following to enable URLs in path-format
-		/*
 		'urlManager'=>array(
 			'urlFormat'=>'path',
 			'showScriptName'=>false,
-			'urlSuffix'=>'.html',
+			//~ 'urlSuffix'=>'.html',
 			'rules'=>array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
 		/*
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
 		// uncomment the following to use a MySQL database
+		
+		'db'=>array(
+			'connectionString' => 'mysql:host=mysql.hostinger.com.ar;dbname=u756927963_mysql',
+			'emulatePrepare' => true,
+			'username' => 'u756927963_unaj',
+			'password' => 'ps@2014--UNAJ',
+			'charset' => 'utf8',
+		),
 		*/
+		
 		'db'=>array(
 			'connectionString' => 'mysql:host=db4free.net;dbname=medicos',
 			'emulatePrepare' => true,
 			'username' => 'proyecto2014',
 			'password' => 'Unaj2014',
 			'charset' => 'utf8',
-		),
-		/*
-		'db'=>array(
-			'connectionString' => 'mysql:host=127.0.0.1;dbname=yiiprsft',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '1234',
-			'charset' => 'utf8',
-		),
-		*/
+		),		
+		//~ 'db'=>array(
+			//~ 'connectionString' => 'mysql:host=127.0.0.1;dbname=yiiprsft',
+			//~ 'emulatePrepare' => true,
+			//~ 'username' => 'root',
+			//~ 'password' => '1234',
+			//~ 'charset' => 'utf8',
+		//~ ),
+		
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
