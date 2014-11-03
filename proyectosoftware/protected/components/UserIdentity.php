f6f8c6b7487ad209fe1bed562f988b830f799fc0
Roberto Devicenzi <rdevicenzi@gmail.com>
2014-11-03 01:27:30 -0300

    Se modifica la clase UserIdentity. Se sobreescribe el método getId() para acceder al id del usuario de la forma Yii::app()->user->id. Se guarda la fila completa (BD) del usuario en un array $this->setState(fila,$fila); para luego acceder a cada campo de la fila (BD) Yii::app()->user->getState(fila)->username. Se hace un update en la BD para el campo fecha_ultimo_login.

--- proyectosoftware/protected/components/UserIdentity.php
+++ proyectosoftware/protected/components/UserIdentity.php
@@ -7,6 +7,8 @@
  */
 class UserIdentity extends CUserIdentity
 {
+	
+	private $_id;
 	/**
 	 * Authenticates a user.
 	 * The example implementation makes sure if the username and password
@@ -30,10 +32,33 @@ class UserIdentity extends CUserIdentity
 			 * si lo comparamos directamente con el password del login nunca va a coincidir
 			 * por eso a $this->password hay que aplicarle el mismo hash 
 			 * 
-			 * si coincide el password y si el usuario tiene su cuenta activa, entonces puede iniciar sesión
+			 * si coincide el password y si el usuario tiene su cuenta activa,
+			 * entonces puede iniciar sesión
 			 */
-			if ($fila->password === sha1($this->password) && $fila->active == 1)
+			if ($fila->password === sha1($this->password) && $fila->active == 1) {
+				
+				$this->_id = $fila->id;
+				
+				/*
+				 * Ya que al momento de autenticar al usuario, traemos su fila completa de la BD.
+				 * Se aprovecha esto dejando sus datos a disposición durante el tiempo que dure su sesión.
+				 * 
+				 * Se almacena el array $fila (BD) del usuario:
+				 * $this->setState('fila',$fila);
+				 * 
+				 * Se puede acceder a cualquier campo de la fila (BD)
+				 * Yii::app()->user->getState('fila')->id
+				 * Yii::app()->user->getState('fila')->username
+				 * Yii::app()->user->getState('fila')->email
+				 * 
+				 **/
+				$this->setState('fila',$fila);
+
+				//~ se hace un update para el campo fecha_ultimo_login de la BD
+				$fila->updateByPk($fila->id, array('fecha_ultimo_login'=>date('Y-m-d')));
+				
 				$this->errorCode=self::ERROR_NONE;
+			 }
 			else
 				$this->errorCode=self::ERROR_PASSWORD_INVALID;
 		}
@@ -44,8 +69,16 @@ class UserIdentity extends CUserIdentity
 		return !$this->errorCode;
 	}
 	
-	
-
+	/*
+	 * Se sobreescribe el método getId() de la clase padre.
+	 * Devuelve el id del usuario
+	 * 
+	 * Se accede de la forma:
+	 * Yii::app()->user->id
+	 **/
+	public function getId(){
+		return $this->_id;
+	}
 
 
 }
