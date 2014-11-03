50d79c857275d88e2aa88618f84281a814bc3566
LeaxGuidi <lg.leax@gmail.com>
2014-11-03 19:39:24 -0300

    se modifico la vista visitar, falta corregir que no aparece el menu

--- proyectosoftware/protected/views/visitas/visitar.php
+++ proyectosoftware/protected/views/visitas/visitar.php
@@ -5,7 +5,17 @@ $this->breadcrumbs=array(
 
 <h1>Pacientes a visitar</h1>
 
-<?php $this->widget('bootstrap.widgets.TbGridView',array(
+<?php 
+	//print_r($model);
+	foreach ($model as $data) {
+		echo "Direccion: ".$data->attributes['address']."<br>";
+		echo "Lat: ".$data->attributes['lat']."<br>";
+		echo "Lon: ".$data->attributes['lon']."<br><br>";
+	}
+
+ ?>
+
+<!--<?php $this->widget('bootstrap.widgets.TbGridView',array(
 	'id'=>'visitas-grid',
 	'dataProvider'=>$model->search(),
 	'filter'=>$model,
@@ -25,4 +35,4 @@ $this->breadcrumbs=array(
 			'class'=>'bootstrap.widgets.TbButtonColumn',
 		),
 	),
-)); ?>
+)); ?>-->
