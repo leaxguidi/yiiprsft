8e020e6c9219b849b0d791d42d4b5de82b664adb
Roberto Devicenzi <rdevicenzi@gmail.com>
2014-11-11 05:54:23 -0300

    Se agrega la vista authenticate para autenticar al usuario a la hora de modificar su perfil.

---
+++ proyectosoftware/protected/views/perfil/authenticate.php
@@ -0,0 +1,71 @@
+<?php 
+    /* Input dialog with Javascript callback */
+    $this->beginWidget('zii.widgets.jui.CJuiDialog', array(
+        'id'=>'mydialog2',
+        'options'=>array(
+            'title'=>'Ingresa tu Contraseña',
+            'autoOpen'=>false,
+            'modal'=>true,
+            //~ 'buttons'=>array(
+                //~ 'Enviar'=>'js:addItem',
+                //~ 'Cancel'=>'js:function(){ $(this).dialog("close");}',
+            //~ ),
+        ),
+    ));?>
+	<?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm',array(
+		'id'=>'usuarios-form',
+		'type'=>'horizontal',
+		'enableClientValidation'=>true,
+		'enableAjaxValidation'=>true,
+	));?>
+	<div class="well">
+		<div class="dialog_input">
+			<input class="span3" 
+			type="password" name="Perfil[password]" id="Perfil_password" onkeypress="return pulsar(event)"
+			value="<?php echo $model->password;  ?>" >
+		</div>
+		<br>
+
+		<button class="btn btn-primary" type="submit" name="yt0">Enviar</button>
+	</div>
+		
+	<?php $this->endWidget('bootstrap.widgets.TbActiveForm');?>
+	<?php $this->endWidget('zii.widgets.jui.CJuiDialog');?>
+
+<?php 
+	echo '<blockquote>';
+	echo '<br>';
+	
+		echo '<hr>';
+		echo '<h5>';
+		echo '<li>';
+		echo CHtml::link('ACTUALIZAR PERFIL', '#', array(
+				'onclick'=>'$("#mydialog2").dialog("open"); return false;'));
+		echo '</li>';
+		echo '</h5>';
+		echo '<hr>';
+		
+		echo '<h5>';
+		echo '<li>';
+		echo CHtml::link(CHtml::encode('VER PERFIL'),array('view'));
+		echo '</li>';
+		echo '</h5>';
+		echo '<hr>';
+	    
+	echo '<br>';
+	echo '<sript>';
+	echo '</blockquote>';
+  	
+?>
+<?php /* include your relevant javascript somewhere */ ?>
+<script type="text/javascript" >
+    var input = document.getElementById('Perfil_password').value;
+    function addItem(){
+        $(this).dialog("close");
+        console.log(document.getElementById('Perfil_password').value);
+        document.getElementById('Perfil_password').value = '';
+        
+        //~ alert( $("#Perfil_password").val() + " has been added");
+    }
+</script>
+
